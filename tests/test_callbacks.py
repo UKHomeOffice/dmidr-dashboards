a640444222139e9dd023c2c98fee569cd1759717
Emma <emma@madetech.com>
2022-10-20 15:29:22 +0100

    Test callback for day selector

--- tests/test_callbacks.py
+++
@@ -1,4 +0,0 @@
-from app.index import login_status
-from app.pages.operational_report.operational_report_body import filter_table_by_day
-
-
---
+++ tests/test_day_selector_row.py
@@ -0,0 +1,39 @@
+from contextvars import copy_context
+from dash._callback_context import context_value
+from dash._utils import AttributeDict
+
+
+from app.pages.operational_report.day_selector_row import day_clicks
+
+
+def test_day_selector_selects_tuesday():
+    def run_callback():
+        context_value.set(AttributeDict(**{"triggered_inputs": [{"prop_id": "tues-btn.n_clicks"}]}))
+        return day_clicks()
+
+    ctx = copy_context()
+    output = ctx.run(run_callback)
+    assert output[0] ==  "Tues"
+    assert output[1:6] == [
+            "day-selector govuk-body",
+            "day-selector--active govuk-body govuk-!-font-weight-bold",
+            "day-selector govuk-body",
+            "day-selector govuk-body",
+            "day-selector govuk-body"
+        ]
+
+def test_day_selector_selects_friday():
+    def run_callback():
+        context_value.set(AttributeDict(**{"triggered_inputs": [{"prop_id": "fri-btn.n_clicks"}]}))
+        return day_clicks()
+
+    ctx = copy_context()
+    output = ctx.run(run_callback)
+    assert output[0] ==  "Fri"
+    assert output[1:6] == [
+            "day-selector govuk-body",
+            "day-selector govuk-body",
+            "day-selector govuk-body",
+            "day-selector govuk-body",
+            "day-selector--active govuk-body govuk-!-font-weight-bold",
+        ]

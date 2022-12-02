16e8e10c4acbb57e22208f3d88979447b120a26c
Rhys <Rhys.Jones@madetech.com>
2022-12-02 10:12:42 +0000

    Add spacing to functions

--- app/data/MPAM/mpam_due_cases.py
+++ app/data/MPAM/mpam_due_cases.py
@@ -5,11 +5,13 @@ from app.data.get_data import create_db_connection
 MPAM_DUE_CASES_QUERY = "SELECT * FROM public.mpam_due_cases"
 MPAM_DUE_CASES_AGGREGATE_QUERY = "SELECT * FROM public.mpam_due_cases_aggregate"
 
+
 def get_mpam_due_cases():
         with create_db_connection() as connection:
             data = pd.read_sql_query(MPAM_DUE_CASES_QUERY, connection)
 
             return data
+
 
 def get_mpam_due_cases_aggregate():
         with create_db_connection() as connection:

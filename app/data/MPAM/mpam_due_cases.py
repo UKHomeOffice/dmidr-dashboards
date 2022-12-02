3751fdeda3af183f8df77a0d70b7b0a0a7e0f23e
Rhys <Rhys.Jones@madetech.com>
2022-12-02 10:23:52 +0000

    Add dummy data for running locally

--- app/data/MPAM/mpam_due_cases.py
+++ app/data/MPAM/mpam_due_cases.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 
 from app.data.get_data import create_db_connection
@@ -5,16 +7,65 @@ from app.data.get_data import create_db_connection
 MPAM_DUE_CASES_QUERY = "SELECT * FROM public.mpam_due_cases"
 MPAM_DUE_CASES_AGGREGATE_QUERY = "SELECT * FROM public.mpam_due_cases_aggregate"
 
+# Dummy data
+blanks = [
+    "blank",
+    "blank",
+    "blank",
+    "blank",
+    "blank",
+    "blank",
+    "blank",
+    "blank",
+    "blank",
+    "blank",
+]
+
+dummy_data = pd.DataFrame(
+    data={
+        "Unit": [
+            "Unit 1",
+            "Unit 2",
+            "Unit 3",
+            "Unit 4",
+            "Unit 5",
+            "Unit 6",
+            "Unit 7",
+            "Unit 8",
+            "Unit 9",
+            "Unit 10",
+        ],
+        "Due": blanks,
+        "Awaiting QA": blanks,
+        "Answered": blanks,
+        "Answered on time": blanks,
+        "Performance": blanks,
+        "Unanswered": blanks,
+    }
+)
+
 
 def get_mpam_due_cases():
-        with create_db_connection() as connection:
-            data = pd.read_sql_query(MPAM_DUE_CASES_QUERY, connection)
 
-            return data
+    if os.environ.get("STAGE") == "local":
+        return dummy_data
+
+    with create_db_connection() as connection:
+        data = pd.read_sql_query(MPAM_DUE_CASES_QUERY, connection)
+
+        return data
 
 
 def get_mpam_due_cases_aggregate():
-        with create_db_connection() as connection:
+    if os.environ.get("STAGE") == "local":
+        return pd.Series(data={
+            "Total due this week": 10,
+            "Total due next 4 weeks": 18,
+            "Total out of service standard": 3,
+            "Total cases": 18
+        })
+
+    with create_db_connection() as connection:
             data = pd.read_sql_query(MPAM_DUE_CASES_AGGREGATE_QUERY, connection)
 
             return data.squeeze()
--- app/pages/due_cases.py
+++ app/pages/due_cases.py
@@ -1,9 +1,8 @@
-import dash
+import dash 
 from dash import html, dcc
 
-from app.pages.closed_cases_comp import *
-from app.pages.open_cases_comp import *
-from app.components import report_header
+from app.components import *
+from app.pages.operational_report import *
 
 dash.register_page(
     __name__, 
@@ -12,7 +11,7 @@ dash.register_page(
 )
 
 layout = html.Div(
-    className="report-background-box govuk-body",
+    className="report-background-box govuk-body", 
     children=[
         html.Div(
             style={"paddingLeft":"10px"},
@@ -29,7 +28,7 @@ layout = html.Div(
             className="custom-tabs-container",
             children=[
                 dcc.Tab(
-                    label="Report One",
+                    label="Cases due this week",
                     className="custom-tab",
                     selected_className="custom-tab--selected",
                     children=[
@@ -37,82 +36,18 @@ layout = html.Div(
                             className="tab-controls",
                             children=[
                                 html.P(
-                                    className="govuk-body",
+                                    className="govuk-body-l",
                                     style={"marginBottom": "0px"},
-                                    children="There is a date here: 10/02/2022",
+                                    children="Controls",
                                 )
                             ],
                         ),
-                        report_header("The reports"),
-                        html.Div(
-                            className="decs-grid-row",
-                            children=[
-                                html.Div(
-                                    style={"padding": "0px 15px"},
-                                    children=[
-                                        html.P(
-                                            className="govuk-body",
-                                            children="Suspendisse potenti. Proin aliquet mi vel viverra faucibus. Quisque a lacus ac diam bibendum placerat. Etiam placerat eros a urna dapibus accumsan. Duis eu ipsum dignissim, sagittis arcu sit amet, tincidunt orci. Donec pulvinar, nibh ac rutrum faucibus, mauris augue malesuada odio, eget luctus turpis justo a nulla. Ut sed ipsum libero. Morbi rutrum, nisi at hendrerit luctus, lacus neque ultrices sem, id pellentesque nibh nunc et turpis. Nam auctor nibh ut orci viverra, vel suscipit neque cursus. Sed cursus nibh eu porttitor interdum. Suspendisse potenti. Aenean eleifend, nisi eget aliquam consequat, odio mauris venenatis purus, id ornare justo tortor a nisl. Proin nec tincidunt nisl. Nullam nec posuere mi. Nulla et erat ultricies, condimentum sapien ut, posuere lacus. Vestibulum ante mauris, pellentesque ac tellus sit amet, fringilla imperdiet nibh.",
-                                        ),
-                                    ],
-                                ),
-                                html.Div(
-                                    className="decs-grid-row",
-                                    style={"marginBottom": "30px"},
-                                    children=[
-                                        html.Div(
-                                            className="decs-grid-column-three-quarters",
-                                            children=[
-                                                html.Div(
-                                                    className="information-box",
-                                                    children=[
-                                                        html.Span(
-                                                            className="govuk-caption-m",
-                                                            children="When are tickets completed through the week",
-                                                        ),
-                                                        html.H3(
-                                                            className="govuk-heading-m",
-                                                            children="Ticket completion",
-                                                        ),
-                                                        example_bar,
-                                                    ],
-                                                )
-                                            ],
-                                        ),
-                                        html.Div(
-                                            className="decs-grid-column-one-quarter",
-                                            children=[
-                                                html.Div(
-                                                    className="information-box",
-                                                    children=[
-                                                        html.Span(
-                                                            className="govuk-caption-m",
-                                                            children="All business targets",
-                                                        ),
-                                                        html.H3(
-                                                            className="govuk-heading-m",
-                                                            children="Performance",
-                                                        ),
-                                                        example_gauge,
-                                                    ],
-                                                )
-                                            ],
-                                        ),
-                                    ],
-                                ),
-                                html.Div(
-                                    className="decs-grid-row",
-                                    children=[
-                                        ticket_details_sec,
-                                        performance_table_sec,
-                                    ],
-                                ),
-                            ],
-                        ),
-                    ],
+                        report_header("Cases due this week"),
+                        operational_report_body
+                    ]
                 ),
                 dcc.Tab(
-                    label="Report Two",
+                    label="Cases due in next 4 weeks",
                     className="custom-tab",
                     selected_className="custom-tab--selected",
                     children=[
@@ -120,46 +55,34 @@ layout = html.Div(
                             className="tab-controls",
                             children=[
                                 html.P(
-                                    className="govuk-body",
+                                    className="govuk-body-l",
                                     style={"marginBottom": "0px"},
-                                    children="There is a date here: 10/02/2022",
+                                    children="Controls",
                                 )
                             ],
                         ),
-                        report_header("The reports"),
-                        html.Div(
-                            className="decs-grid-row",
-                            children=[
-                                html.Div(
-                                    style={"padding": "0px 15px"},
-                                    children=[
-                                        html.P(
-                                            className="govuk-body",
-                                            children="Suspendisse potenti. Proin aliquet mi vel viverra faucibus. Quisque a lacus ac diam bibendum placerat. Etiam placerat eros a urna dapibus accumsan. Duis eu ipsum dignissim, sagittis arcu sit amet, tincidunt orci. Donec pulvinar, nibh ac rutrum faucibus, mauris augue malesuada odio, eget luctus turpis justo a nulla. Ut sed ipsum libero. Morbi rutrum, nisi at hendrerit luctus, lacus neque ultrices sem, id pellentesque nibh nunc et turpis. Nam auctor nibh ut orci viverra, vel suscipit neque cursus. Sed cursus nibh eu porttitor interdum. Suspendisse potenti. Aenean eleifend, nisi eget aliquam consequat, odio mauris venenatis purus, id ornare justo tortor a nisl. Proin nec tincidunt nisl. Nullam nec posuere mi. Nulla et erat ultricies, condimentum sapien ut, posuere lacus. Vestibulum ante mauris, pellentesque ac tellus sit amet, fringilla imperdiet nibh.",
-                                        ),
-                                    ],
-                                ),
-                            ],
-                        ),
-                        html.Div(
-                            className="decs-grid-row",
-                            children=[
-                                html.Div(
-                                    className="decs-grid-column-two-thirds",
-                                    children=[
-                                        example_case_count,
-                                        completion_time_table,
-                                    ],
-                                ),
-                                html.Div(
-                                    className="decs-grid-column-one-third",
-                                    children=[example_pie],
-                                ),
-                            ],
-                        ),
-                    ],
+                        report_header("Cases due in the next 4 weeks"),
+                    ]
                 ),
-            ],
-        )
-    ],
+                dcc.Tab(
+                    label="Out of service standard cases",
+                    className="custom-tab",
+                    selected_className="custom-tab--selected",
+                    children=[
+                        html.Div(
+                            className="tab-controls",
+                            children=[
+                                html.P(
+                                    className="govuk-body-l",
+                                    style={"marginBottom": "0px"},
+                                    children="Controls",
+                                )
+                            ],
+                        ),
+                        report_header("Out of service standard cases"),
+                    ]
+                )
+            ]
+        ), 
+    ]
 )

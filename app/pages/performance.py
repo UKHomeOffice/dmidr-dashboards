c4e9dd30d5f922f055bfc4da340a62abd055eee3
Rhys <Rhys.Jones@madetech.com>
2023-01-04 10:47:58 +0000

    Updating performance report to use report base

--- app/pages/performance.py
+++ app/pages/performance.py
@@ -3,6 +3,7 @@ from dash import html, dcc
 
 from data.MPAM.mpam_performance_cases import get_mpam_performance_cases, get_mpam_performance_by_date
 from app.pages.performance_cases import *
+from app.pages.report_base import report_base
 from app.components import report_header
 
 
@@ -12,61 +13,30 @@ dash.register_page(
     path="/performance-summary"
 )
 
-layout = html.Div(
-    className="report-background-box govuk-body",
-    children=[
-        html.Hr(
-            className="decs-section-break"
-        ),
-        html.Div(
-            style={"paddingLeft": "10px"},
-            children=[
-                html.A(
-                    className="govuk-back-link govuk-link--no-underline govuk-!-font-size-19",
-                    style={
-                        "paddingLeft": "20px"
-                    },
-                    children="Back to home",
-                    href="/"
-                ),
-            ]
-        ),
-        html.Div(
-                className="tab-controls",
-                children=[
-                    html.P(
-                        className="govuk-body-l",
-                        style={"marginBottom": "0px"},
-                        children=[
-                            "Controls",
-                        ],
-                    )
-                ],
-            ),
-        report_header("Performance Summary"),
-        html.Div(
-            className="decs-grid-row",
-            children=[
-                html.Div(
-                    className="decs-grid-row",
-                    style={
-                        "padding": "0px 15px"
-                    },
-                    children=performance_bar(get_mpam_performance_by_date(), plot_title="Performance"),
-                ),
-            ],
-        ),
-        html.Div(
-            className="decs-grid-row",
-            children=[
-                html.Div(
-                    className="decs-grid-row",
-                    style={
-                        "padding": "0px 15px"
-                    },
-                    children=performance_table(get_mpam_performance_cases()),
-                ),
-            ],
-        ),
-    ]
-),
+layout = report_base(title="Performance Summary",
+                     body=[
+                         html.Div(
+                             className="decs-grid-row",
+                             children=[
+                                 html.Div(
+                                     className="decs-grid-row",
+                                     style={
+                                         "padding": "0px 15px"
+                                     },
+                                     children=performance_bar(get_mpam_performance_by_date(), plot_title="Performance"),
+                                 ),
+                             ],
+                         ),
+                         html.Div(
+                             className="decs-grid-row",
+                             children=[
+                                 html.Div(
+                                     className="decs-grid-row",
+                                     style={
+                                         "padding": "0px 15px"
+                                     },
+                                     children=performance_table(get_mpam_performance_cases()),
+                                 ),
+                             ],
+                         ),
+                     ])

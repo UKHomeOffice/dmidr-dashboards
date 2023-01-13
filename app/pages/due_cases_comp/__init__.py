cc0830102b85ae664aca8a37641e18addbf2f60b
Rhys <Rhys.Jones@madetech.com>
2023-01-13 12:41:06 +0000

    Remove the import for due cases base page from init

--- app/pages/due_cases_comp/__init__.py
+++ app/pages/due_cases_comp/__init__.py
@@ -1,4 +1,2 @@
-# from .due_case_body import due_case_body
 from .day_selector_row import day_selector_row_func
 from .counting_section import counting_section
-from .due_cases_base_page import due_cases_base_page
